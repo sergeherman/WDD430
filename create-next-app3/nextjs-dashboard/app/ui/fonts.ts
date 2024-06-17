a254a95d4bb360afe87bcc516484ce3d7577d638
Admin <herman.sergi@gmail.com>
2024-06-17 23:11:42 +0200

    Completed Chapters 1 through 3 of the tutorial

---
+++ create-next-app3/nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'] , // Adjust based on your needs
+    weight: ['400', '700'], // Adjust weight (e.g., '700' for bold)
+  });
